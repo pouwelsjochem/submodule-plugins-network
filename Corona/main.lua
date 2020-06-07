fc65d4f21bf2b268d2c0edd4647de9d996b97515
Jochem Pouwels <pouwelsjochem@gmail.com>
2020-06-07 20:07:12 +0200

    Imported all changes of previous fork
    Removed frame trimming and added crash when providing 0 frames
    Simplified SpriteObject and re-enabled frames param in imageSheet
    Simplified welcome-screen so it won't need widgets anymore
    Removed widget.* library and some other folders/plugins
    Removed graphics v1 compatability
    Removed physics, particles and all kinds of related code
    Removed AUTO_INCLUDE_MONETIZATION_PLUGIN
    Removed deprecated stageWidth and stageHeight
    Removed deprecated stageBounds
    Removed deprecated xOrigin and yOrigin
    Removed deprecated setColor and width on lineObject
    Removed submodule composer
    Removed submodule platform/emscripten
    Removed submodule platform/linux
    Removed submodule timer
    Removed submodule transition
    Removed submodule enterprise
    Removed multisampling
    Removed wireframe drawMode
    Removed some legacy syntaxes
    Removed statusBar handling (now always hidden)
    Remove Nook platform
    Removed GPS and Compass functionality
    Improved some matrix syntax for readability
    Cleaned up a lot of dead code

--- Corona/main.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- New network.request() / network.cancel() test suite
--------------------------------------------------------------------------------
-
--- TODO: Remove this once we this is built into the simulator
-
-
-local platformName = system.getInfo( "platformName" )
-
